5c8974ed2b98968550ae715ef188cd4575386601
lkr27 <lkr27@uclive.ac.nz>
2020-05-12 20:24:46 +1200

    enable overflow, disable tabletopf for mobile

--- js/master.js
+++ js/master.js
@@ -5,6 +5,12 @@ window.addEventListener('load', function () {
 function init() {
     document.body.classList.add('no-curtains')
 
-    var tableTop = new Tabletop()
+    if (window.innerWidth > 480) {
+        var tableTop = new Tabletop()
+    } else {
+        document.getElementsByTagName('body')[0].style.overflowX = 'auto'
+        document.getElementsByTagName('body')[0].style.overflowY = 'auto'
+    }
+
     loadTransitions()
 }
