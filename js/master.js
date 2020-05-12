c52e7d46d8526d9d34bcc183746e423d5fcd0de8
lkr27 <lkr27@uclive.ac.nz>
2020-05-12 12:51:20 +1200

    changed the way elements are sized
    bit cleaner this way (css based)

--- js/master.js
+++ js/master.js
@@ -1,7 +1,10 @@
 window.addEventListener('load', function () {
+    init()
+})
+
+function init() {
     document.body.classList.add('no-curtains')
 
     var tableTop = new Tabletop()
-
     loadTransitions()
-})
+}
